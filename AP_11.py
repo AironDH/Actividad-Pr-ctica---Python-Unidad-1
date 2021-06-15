d634bdc5a2b9f1b085a34dcada7c6afa7a17000f
AironDH <airondh@gmail.com>
2021-06-15 16:03:32 -0300

    Refinamiento del algoritmo AP_11.py

--- AP_11.py
+++ AP_11.py
@@ -5,11 +5,11 @@ num = int(input("Escribe un número y te digo si es primo\n"))
 raiz = num**0.5
 primos = [2,3,5,7,11,13,17,19]
 
-if num < 20 and num in primos:
+if num in primos:
     print("Tu número es primo")
 elif num < 20 and not num in primos:
     print("Tu número no es primo")
-elif num > 19:
+else:
     for div in range(2,int(round(raiz) + 1)):
         if num%div == 0:
             print("Su número no es primo")
